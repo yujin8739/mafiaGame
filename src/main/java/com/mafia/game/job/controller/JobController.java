f1be397c76ab0fafeded713862a5e368781cb784
Yoush01 <yoush100@kyonggi.ac.kr>
2025-07-04 09:38:26 +0900

    직업부여 로직 구현 중

--- src/main/java/com/mafia/game/job/controller/JobController.java
+++ src/main/java/com/mafia/game/job/controller/JobController.java
@@ -1,5 +1,8 @@
 package com.mafia.game.job.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,11 +25,33 @@ public class JobController {
 	@RequestMapping(value = "/employment",
 					produces = "text/html;charset=UTF-8")
 	public String jobEmployment(HttpSession session) {
-		//직업 테이블에서
+		//플레이어 명단(리스트)
+		List<String> pList = new ArrayList<>();
+		pList.add("player1");
+		pList.add("player2");
+		pList.add("player3");
+		pList.add("player4");
+		pList.add("player5");
+		pList.add("player6");
+		//플레이어 수
+		int pCount = pList.size();
 		
+		//플레이어 수에 따라 분기(6, 8, 10)
+		//각 플레이어 수에 맞는 직업 테이블에서 필수 직업과 선택 직업 조회
+		if(pCount < 8) {
+			List<Integer> ejList = service.essentialJob6();
+			List<Integer> ojList = service.optionalJob6();
+		}else if(pCount < 10) {
+			//8인 이상 게임 직업 테이블에서 직업 목록 조회
+		}
 		
 		
 		return "직업 랜덤 부여 성공";
 	}
+	
+	
+	
+	
+	
 	
 }
