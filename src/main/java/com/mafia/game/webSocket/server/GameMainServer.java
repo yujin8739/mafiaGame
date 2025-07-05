ac9289ba1d95c69060bef5971737c152acc7a42d
yujin8739 <yujin8739@naver.com>
2025-07-05 19:20:44 +0900

    게임방에서 메시지 발송시 저장

--- src/main/java/com/mafia/game/webSocket/server/GameMainServer.java
+++ src/main/java/com/mafia/game/webSocket/server/GameMainServer.java
@@ -1,9 +1,16 @@
 package com.mafia.game.webSocket.server;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.socket.*;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mafia.game.game.model.vo.Message;
 import com.mafia.game.member.model.vo.Member;
 
 
@@ -32,11 +39,28 @@ public class GameMainServer extends TextWebSocketHandler {
     
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        int roomNo = (int) session.getAttributes().get("roomNo");
+        // 메시지 파싱
+        ObjectMapper mapper = new ObjectMapper();
+        Message msg = mapper.readValue(message.getPayload(), Message.class);
 
+        // DB 저장
+        msg.setMsgNo(UUID.randomUUID().toString());
+        msg.setChatDate(new Date());
+        
+        roomManager.sendMessage(msg);
+        // 해당 방 세션 목록 가져오기
+        int roomNo = msg.getRoomNo();
+        
+        Map<String, Object> payload = new HashMap<>();
+        payload.put("sender", msg.getUserName()); // nickName
+        payload.put("content", msg.getMsg());     // 메시지 본문
+
+        String json = mapper.writeValueAsString(payload);
+
+        // 브로드캐스트
         for (WebSocketSession s : roomManager.getSessions(roomNo)) {
             if (s.isOpen()) {
-                s.sendMessage(message);
+                s.sendMessage(new TextMessage(json));
             }
         }
     }
