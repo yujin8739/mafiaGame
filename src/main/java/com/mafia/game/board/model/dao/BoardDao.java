3181cc1a9570f71a753dd0ea1fdd4413ba433f3a
2swan07 <ceolsh1234@gmail.com>
2025-07-05 18:40:36 +0900

    7/5 커밋&푸쉬

--- src/main/java/com/mafia/game/board/model/dao/BoardDao.java
+++ src/main/java/com/mafia/game/board/model/dao/BoardDao.java
@@ -8,6 +8,7 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.mafia.game.board.model.vo.Board;
+import com.mafia.game.board.model.vo.BoardFile;
 import com.mafia.game.common.model.vo.PageInfo;
 
 @Repository
@@ -22,6 +23,26 @@ public class BoardDao {
 		int offset = (pi.getCurrentPage()-1) * limit;
 		
 		return (ArrayList)sqlSession.selectList("boardMapper.boardList", filterMap, new RowBounds(offset,limit));
+	}
+
+	public Board loungeBoardDetail(SqlSessionTemplate sqlSession, int boardNo) {
+		return sqlSession.selectOne("boardMapper.loungeBoardDetail",boardNo);
+	}
+
+	public int writeLoungeBoard(SqlSessionTemplate sqlSession, Board board) {
+		return sqlSession.insert("boardMapper.writeLoungeBoard",board);
+	}
+
+	public int getBoardNo(SqlSessionTemplate sqlSession) {
+		return sqlSession.selectOne("boardMapper.getBoardNo");
+	}
+
+	public int insertFile(SqlSessionTemplate sqlSession,BoardFile file) {
+		return sqlSession.insert("boardMapper.insertFile",file);
+	}
+
+	public int increaseCount(SqlSessionTemplate sqlSession,int boardNo) {
+		return sqlSession.update("boardMapper.increaseCount",boardNo);
 	}
 
 	
