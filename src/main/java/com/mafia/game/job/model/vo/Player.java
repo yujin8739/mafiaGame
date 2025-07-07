2362f719e2748e8369dbeadb6fb5cacc7c20ecfb
Yoush01 <yoush100@kyonggi.ac.kr>
2025-07-07 11:08:41 +0900

    Merge branch 'main' of https://github.com/yujin8739/mafiaGame.git into sh

---
+++ src/main/java/com/mafia/game/job/model/vo/Player.java
@@ -0,0 +1,18 @@
+package com.mafia.game.job.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class Player {
+	
+	private int playerNo;
+	private int jobNo;
+	private int roomNo;
+	
+}
